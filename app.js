4cf3fd2b1533569555a075c5c166970e3a9a31f4
klarstil <klarstil@googlemail.com>
2013-08-20 15:50:08 +0200

    Updated: Added VR.js functionality and send the rotation of the Oculus Rift to the server using socket.io

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 var http = require('http'),
     droneStream = require('dronestream'),
-    httpServer;
+    io = require('socket.io'),
+    httpServer, socket;
 
 httpServer = http.createServer(function(req, res) {
    require('fs').createReadStream(__dirname + '/index.html').pipe(res);
 });
-
+socket = io.listen(8080);
 droneStream.listen(httpServer);
 httpServer.listen(8000);
-console.log('Server started at localhost:8000');
+
+socket.on('connection', function(client) {
+	console.log("Socket server is start on port 8080");
+	
+	client.on('rotation', function(event) {
+		console.log('Got message from client', event);
+	});
+});
+
+console.log('Server is start on 8000');
